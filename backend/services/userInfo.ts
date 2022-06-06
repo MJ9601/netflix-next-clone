7c6310b6d15ae850e81d53b687f954c28387974e
mj khodadadi <m_javad_96@yahoo.com>
2022-06-06 01:28:35 -0700

    6/6 127am {end of setting requests}

--- backend/services/userInfo.ts
+++ backend/services/userInfo.ts
@@ -1,3 +1,4 @@
+import { MovieObjectOnPage, MovieRespObj } from "../../typing";
 import { connectToDB } from "../utils/mongodbConn";
 
 export const createNewUserInfo = async ({
@@ -21,14 +22,77 @@ export const createNewUserInfo = async ({
   }
 };
 
-export const addToUserWishlist = async ({ body, userEmail }: { body: any , userEmail:string}) => {
-  const {db} = await connectToDB();
+export const updateUserWishlist = async ({
+  body,
+  userEmail,
+}: {
+  body: any;
+  userEmail: string;
+}) => {
+  const { db } = await connectToDB();
   try {
-    const updatedWishlist = await db.collection("userInfos").updateOne({owner: userEmail}, )
+    const userInfo = await db
+      .collection("userInfos")
+      .findOne({ owner: userEmail });
+    const video = userInfo.wishlist.find(
+      (videoInfo: MovieObjectOnPage | MovieRespObj) =>
+        videoInfo?.id == body.video.id
+    );
+    if (!video) {
+      const updatedWishlist = await db
+        .collection("userInfos")
+        .updateOne(
+          { owner: userEmail },
+          { $set: { wishlist: [...userInfo.wishlist, body.video] } },
+          { new: true }
+        );
+      return { updatedWishlist };
+    } else {
+      const updatedWishlist = await db.collection("userInfos").updateOne(
+        { owner: userEmail },
+        {
+          $set: {
+            wishlist: [
+              ...userInfo.wishlist.filter(
+                (videoInfo: MovieObjectOnPage | MovieRespObj) =>
+                  videoInfo?.id != body.video.id
+              ),
+            ],
+          },
+        },
+        { new: true }
+      );
+      return { updatedWishlist };
+    }
   } catch (error) {
-    
+    return { error };
   }
 };
 
-export const getUserInfos = async() => {}
-export const updateUserPlanInfo = async ({ body }: { body: any }) => {};
+export const getUserInfos = async ({ userEmail }: { userEmail: string }) => {
+  const { db } = await connectToDB();
+  try {
+    const userInfos = db.collection("userInfos").findOne({ owner: userEmail });
+    return { userInfos };
+  } catch (error) {
+    return { error };
+  }
+};
+
+export const updateUserPlanInfo = async ({
+  body,
+  userEmail,
+}: {
+  body: any;
+  userEmail: string;
+}) => {
+  const { db } = await connectToDB();
+  try {
+    const updatedPlan = await db
+      .collection("userInfos")
+      .updateOne({ owner: userEmail }, { $set: { plan: body.plan } });
+    return { updatedPlan };
+  } catch (error) {
+    return { error };
+  }
+};
--- pages/api/userInfo/index.ts
+++ pages/api/userInfo/index.ts
@@ -1,5 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { decode } from "next-auth/jwt";
+import {
+  createNewUserInfo,
+  getUserInfos,
+  updateUserPlanInfo,
+  updateUserWishlist,
+} from "../../../backend/services/userInfo";
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,4 +17,32 @@ export default async function handler(
     token: cookies["next-auth.session-token"],
     secret: String(process.env.NEXTAUTH_SECRET),
   });
+
+  if (method == "POST") {
+    const { newUserInfo, error } = await createNewUserInfo({
+      body,
+      userEmail: String(decoded?.email),
+    });
+    !error ? res.status(400).send(error) : res.status(201).send(newUserInfo);
+  } else if (method == "GET") {
+    const { error, userInfos } = await getUserInfos({
+      userEmail: String(decoded?.email),
+    });
+
+    !error ? res.status(400).send(error) : res.status(201).send(userInfos);
+  } else if (method == "PUT") {
+    const { error, updatedWishlist } = await updateUserWishlist({
+      body,
+      userEmail: String(decoded?.email),
+    });
+    !error
+      ? res.status(400).send(error)
+      : res.status(201).send(updatedWishlist);
+  } else if (method == "PATCH") {
+    const { error, updatedPlan } = await updateUserPlanInfo({
+      body,
+      userEmail: String(decoded?.email),
+    });
+    !error ? res.status(400).send(error) : res.status(201).send(updatedPlan);
+  }
 }
